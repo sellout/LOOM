5af11bc0ad837f4a12df36218f3dfc91750e8d0c
Greg Pfeil <greg@technomadic.org>
2011-04-26 10:23:46 -0400

    Genericize what little there is of the objects API that's not already generic.

--- src/objects.lisp
+++ src/objects.lisp
@@ -1,5 +1,5 @@
 (loom.internals:defpackage objects
-  (:use #:cl)
+  (:use #:data-and-control-flow #:loom.internals)
   (:export #:function-keywords
            #:ensure-generic-function
            #:allocate-instance
@@ -39,4 +39,59 @@
            #:class-name
            #:class-of
            #:unbound-slot
-           #:unbound-slot-instance))
+           #:unbound-slot-instance)
+  (:import-from #:cl
+                #:function-keywords
+                #:allocate-instance
+                #:reinitialize-instance
+                #:shared-initialize
+                #:update-instance-for-different-class
+                #:update-instance-for-redefined-class
+                #:change-class
+                #:slot-missing
+                #:slot-unbound
+                #:method-qualifiers
+                #:no-applicable-method
+                #:no-next-method
+                #:remove-method
+                #:make-instance
+                #:make-instances-obsolete
+                #:make-load-form
+                #:with-accessors
+                #:with-slots
+                #:defclass
+                #:defgeneric
+                #:defmethod
+                #:find-class ; TODO: make sure this is generic
+                #:next-method-p
+                #:call-method #:make-method
+                #:call-next-method
+                #:compute-applicable-methods
+                #:define-method-combination
+                #:find-method
+                #:add-method
+                #:initialize-instance
+                #:class-name
+                #:unbound-slot))
+
+(cl:in-package #:objects)
+
+(make-generic ensure-generic-function
+              (function-name
+               &key argument-precedence-order declare documentation environment
+               generic-function-class lambda-list method-class
+               method-combination))
+
+(make-generic slot-boundp (instance slot-name))
+
+(make-generic slot-exists-p (object slot-name))
+
+(make-generic slot-makunbound (instance slot-name))
+
+(make-generic slot-value (object slot-name))
+
+(make-generic make-load-form-saving-slots (object &key slot-names environment))
+
+(make-generic class-of (object))
+
+(make-generic unbound-slot-instance (condition))
